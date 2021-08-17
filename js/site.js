9e73a232b8efeef48243f950d0e1efe96d6d2b06
Arthur Redmond <19720427+ArthurRedmond@users.noreply.github.com>
2021-08-17 14:44:06 -0400

    Update The Code text

--- js/site.js
+++ js/site.js
@@ -1,4 +1,38 @@
-//Call HelloWorld 
-function HelloWorld() {
-    alert("Hello World");
+function getValues() {
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
+
+
+    startValue = parseInt(startValue);
+    endValue = parseInt(endValue);
+
+    if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
+        let numbers = generateNumbers(startValue, endValue);
+        displayNumbers(numbers)
+    } else {
+        alert("You must enter integers")
+    }
+}
+
+function generateNumbers(startValue, endValue) {
+    let numbers = [];
+    for (let i = startValue; i <= endValue; i++) {
+        numbers.push(i);
+    }
+    return numbers;
+}
+
+function displayNumbers(numbers) {
+    let templateRows = "";
+    let className = "";
+    for (let i = 0; i < numbers.length; i++) {
+        let number = numbers[i];
+        if (number % 2 == 0) {
+            className = "even";
+        } else {
+            className = "odd";
+        }
+        templateRows += `<tr><td class="${className}">${number}</td></tr>`
+    }
+    document.getElementById("results").innerHTML = templateRows;
 }
